330668058b388d9aba77befe6554108c744bf2f1
dvdgd <dvd.gd762@gmail.com>
2021-12-15 14:53:14 -0300

    implemented categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from "express";
 
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 const categoriesRoute = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoute.post("/", (req, res) => {
-  res.status(501).json({ error: "Method not implemnted" });
+  const { name, description } = req.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    throw new Error("Category already exists");
+  }
+
+  categoriesRepository.create({ name, description });
+
+  res.status(201).send();
 });
 
 categoriesRoute.get("/", (req, res) => {
-  res.status(501).json({ error: "Method not implemnted" });
+  const categories = categoriesRepository.get();
+  res.json({ categories });
 });
 
 export { categoriesRoute };
