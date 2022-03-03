4c74360c691c4c9e77f64f45c77dffedbfa7e457
dvdgd <dvd.gd762@gmail.com>
2022-03-02 23:28:08 -0300

    feat: specifications using database connection

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController..ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController..ts
@@ -4,16 +4,20 @@ import { container } from "tsyringe";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  handle(req: Request, res: Response) {
-    const { name, description } = req.body;
+  async handle(req: Request, res: Response) {
+    try {
+      const { name, description } = req.body;
 
-    const createSpecificationUseCase = container.resolve(
-      CreateSpecificationUseCase
-    );
+      const createSpecificationUseCase = container.resolve(
+        CreateSpecificationUseCase
+      );
 
-    createSpecificationUseCase.execute({ name, description });
+      await createSpecificationUseCase.execute({ name, description });
 
-    return res.status(201).send();
+      return res.status(201).send();
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
   }
 }
 
