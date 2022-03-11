ddba0ecaa8294919b2db3e9bbedc922cda3e71ed
dvdgd <dvd.gd762@gmail.com>
2022-03-11 19:13:08 -0300

    feat: hashing and salting user password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -10,16 +11,26 @@ class CreateUserUseCase {
     private usersRepository: IUsersRepository
   ) {}
 
-  async execute(data: ICreateUserDTO): Promise<void> {
-    const userAlreadyExists = await this.usersRepository.findByEmail(
-      data.email
-    );
+  async execute({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
       throw new Error("User already exists");
     }
 
-    await this.usersRepository.create(data);
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license,
+    });
   }
 }
 
