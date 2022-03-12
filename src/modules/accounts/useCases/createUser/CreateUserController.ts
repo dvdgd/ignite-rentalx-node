af36c70debb3c88a42eed14b153e0c5ff887d0cc
dvdgd <dvd.gd762@gmail.com>
2022-03-12 01:33:21 -0300

    refactor: useCase createUser, error handling

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -5,21 +5,17 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
   async handle(req: Request, res: Response): Promise<Response> {
-    try {
-      const { name, email, driver_license, password } = req.body;
-      const createUserUseCase = container.resolve(CreateUserUseCase);
+    const { name, email, driver_license, password } = req.body;
+    const createUserUseCase = container.resolve(CreateUserUseCase);
 
-      await createUserUseCase.execute({
-        name,
-        email,
-        driver_license,
-        password,
-      });
+    await createUserUseCase.execute({
+      name,
+      email,
+      driver_license,
+      password,
+    });
 
-      return res.status(201).send();
-    } catch (error) {
-      return res.status(500).send(error.message);
-    }
+    return res.status(201).send();
   }
 }
 
