c7c74a4c2afd7066884d781e9b01dd60568483dd
dvdgd <dvd.gd762@gmail.com>
2022-03-14 21:50:13 -0300

    feat: finding user by id

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -20,6 +20,10 @@ class UsersRepository implements IUsersRepository {
   async findByEmail(email: string): Promise<User> {
     return this.repository.findOne({ email });
   }
+
+  async findById(id: string): Promise<User> {
+    return this.repository.findOne({ id });
+  }
 }
 
 export { UsersRepository };
