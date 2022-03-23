10eee19d89c40d2928c6a273f4a8dce17e05d0c7
dvdgd <dvd.gd762@gmail.com>
2022-03-23 17:57:38 -0300

    tests: created categoriesRepository in memory for the tests

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async get(): Promise<Category[]> {
+    const allCategories = this.categories;
+    return allCategories;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const categoryByName = this.categories.find(
+      (category) => category.name === name
+    );
+    return categoryByName;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, {
+      name,
+      description,
+    });
+
+    this.categories.push(newCategory);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
