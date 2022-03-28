efbd7f81056e2751210935219b4dc8d116fead34
dvdgd <dvd.gd762@gmail.com>
2022-03-28 01:05:08 -0300

    infra: configuring paths of common folders to make the project more readable

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
+
 export default {
   clearMocks: true,
   coverageProvider: "v8",
   bail: true,
   preset: "ts-jest",
   testMatch: ["**/*.spec.ts"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src",
+  }),
 };
