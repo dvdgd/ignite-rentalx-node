81077bf6a304548a5b34016182052a718e9e1f45
dvdgd <dvd.gd762@gmail.com>
2022-03-28 00:33:48 -0300

    tests: created tests for AuthenticateUserUseCase

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { UnauthenticatedError } from "../../../../errors/UnauthenticatedError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+describe("Authenticate User", () => {
+  let authenticateUserUseCase: AuthenticateUserUseCase;
+  let usersRepositoryInMemory: UsersRepositoryInMemory;
+  let createUserUseCase: CreateUserUseCase;
+
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("Should be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      email: "user@test.com",
+      password: "1234",
+      name: "user test",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate with a wrong password", async () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "000123",
+        email: "user@test.com",
+        password: "1234",
+        name: "user test",
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "wrongPassword",
+      });
+    }).rejects.toBeInstanceOf(UnauthenticatedError);
+  });
+
+  it("Should not be able to authenticate with nonexists user", async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "thisUser@not.exists",
+        password: "wrongPassword",
+      });
+    }).rejects.toBeInstanceOf(UnauthenticatedError);
+  });
+});
